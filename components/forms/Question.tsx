bec8f4beff99cf45a8a30560d77aefdbdf2ba283
Mukhammad <yekut1515@yandex.ru>
2024-06-25 15:47:58 +0300

    create: installing Editor from Tiny.cloud

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,6 @@
 "use client"
-
+import { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -18,6 +19,8 @@ import {QuestionsSchema} from "@/lib/validations";
 
 
 const Question = () => {
+    const editorRef = useRef(null);
+
     // 1. Define your form.
     const form = useForm<z.infer<typeof QuestionsSchema>>({
         resolver: zodResolver(QuestionsSchema),
@@ -95,7 +98,26 @@ const Question = () => {
                             <FormControl
                                 className='mt-3.5'
                             >
-                                {/*TODO: add an Editor component*/}
+                                <Editor
+                                    apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                                    onInit={(_evt, editor) =>
+                                        //@ts-ignore
+                                        editorRef.current = editor}
+                                    initialValue=""
+                                    init={{
+                                        height: 350,
+                                        menubar: false,
+                                        plugins: [
+                                            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                                            'anchor', 'searchreplace', 'visualblocks', 'fullscreen',
+                                            'insertdatetime', 'media', 'table', 'codesample',
+                                        ],
+                                        toolbar: 'undo redo | ' +
+                                            'codesample | bold italic forecolor | alignleft aligncenter ' +
+                                            'alignright alignjustify | bullist numlist ',
+                                        content_style: 'body { font-family:Inter; font-size:16px }'
+                                    }}
+                                />
                             </FormControl>
                             <FormDescription
                                 className='body-regular mt-2.5 text-light-500'
