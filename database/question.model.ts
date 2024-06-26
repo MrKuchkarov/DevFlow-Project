2be002fa3db4f2b9fdc3d85f15b2a76f2cc5c341
Mukhammad <yekut1515@yandex.ru>
2024-06-26 16:17:06 +0300

    create: create new Schemes have emerged such as user, question and tag

---
+++ database/question.model.ts
@@ -0,0 +1,29 @@
+import { Schema, models, model, Document } from 'mongoose';
+
+export type TQuestion = Document & {
+    title: string;
+    content: string;
+    tags: Schema.Types.ObjectId[];
+    views: number;
+    upvotes: Schema.Types.ObjectId[];
+    downvotes: Schema.Types.ObjectId[];
+    author: Schema.Types.ObjectId;
+    answers: Schema.Types.ObjectId[];
+    createdAt: Date;
+};
+
+const QuestionSchema = new Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
+    views: { type: Number, default: 0 },
+    upvotes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    downvotes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    answers: [{ type: Schema.Types.ObjectId, ref: 'Answers' }],
+    createdAt: { type: Date, default: Date.now }
+})
+
+const Question = models.Question || model<TQuestion>('Question', QuestionSchema);
+
+export default Question;
